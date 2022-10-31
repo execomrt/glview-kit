ab4eb47fc533d526616aa9116ebb56414f2191a8
mspassport <sourceforge@realtech-vr.com>
2022-10-31 10:49:49 -0400

    Updated for GLVIEW 6.3.7 +

--- src/oevTest.cpp
+++ src/oevTest.cpp
@@ -4,13 +4,13 @@
 ; *
 ; * 	Description :	OpenGL Rendering Tests
 ; *
-; * 	Copyright (C) Realtech VR 2000 - 2021 - http://www.realtech-vr.com/glview
+; * 	Copyright (C) Realtech VR 2000 - 2022 - https://www.realtech-vr.com/glview
 ; *
 ; * 	Permission to use, copy, modify, distribute and sell this software
 ; * 	and its documentation for any purpose is hereby granted without fee,
 ; * 	provided that the above copyright notice appear in all copies and
 ; * 	that both that copyright notice and this permission notice appear
-; * 	in supporting documentation.  Realtech makes no representations
+; * 	in supporting documentation.  Realtech VR makes no representations
 ; * 	about the suitability of this software for any purpose.
 ; * 	It is provided "as is" without express or implied warranty.
 ; *
@@ -104,22 +104,36 @@ static inline std::string CreateElement(const std::string& name, const std::stri
 /// <param name="transparency"></param>
 /// <param name="user_clip_pane"></param>
 /// <param name="multisampling"></param>
-/// <param name="anisotropy"></param>
+/// <param name="maxAnisotropy"></param>
 /// <param name="texture_load"></param>
 /// <param name="pixel_format"></param>
 /// <param name="scene"></param>
 /// <param name="fbformat"></param>
 /// <param name="test_duration"></param>
 /// <returns></returns>
-int run_rendering_test(ovRenderer renderer, int fullscreen, int width, int height, int vsync, int fog, int transparency, int user_clip_pane, string multisampling, string anisotropy, string texture_load, string pixel_format, string scene, string fbformat, int test_duration)
+static int run_rendering_test(ovRenderer renderer,
+	int debug,
+	int fullscreen,
+	int width,
+	int height,
+	int vsync,
+	int fog,
+	int transparency,
+	int user_clip_pane,
+	const string& multisampling,
+	const string& maxAnisotropy,
+	const string& texture_load,
+	const string& pixel_format,
+	const string& scene,
+	const string& fbformat,
+	int test_duration)
 {
-	scene = "0";
 	std::string szXml;
 	int option = 0;
 	if (fullscreen) {
-		option |= (WGLDIAG_OPTION_FS);
+		option |= (WGLDIAG_OPTION_FS | WGLDIAG_OPTIONS_FS_EX);
 	}
-	else {
+	if (debug) {
 		option |= (WGLDIAG_OPTION_DEBUG);
 	}
 	if (vsync) {
@@ -134,13 +148,11 @@ int run_rendering_test(ovRenderer renderer, int fullscreen, int width, int heigh
 	if (user_clip_pane) {
 		option |= (WGLDIAG_OPTION_CLIP_PLANE);
 	}
-	if (fbformat == "FLOAT") {
-		option |= (WGLDIAG_OPTION_FB_FLOAT32); // Enable HDR
-	}
 	szXml = szXml + CreateElement("option", std::to_string(option));
+	szXml = szXml + CreateElement("fbformat", fbformat);
 	szXml = szXml + CreateElement("duration", std::to_string(test_duration));
 	szXml = szXml + CreateElement("multisample", multisampling);
-	szXml = szXml + CreateElement("anisotropy", anisotropy);
+	szXml = szXml + CreateElement("anisotropy", maxAnisotropy);
 	szXml = szXml + CreateElement("texturelod", texture_load);
 	szXml = szXml + CreateElement("displaymode", std::to_string(GetDisplayMode(width, height)));
 	szXml = szXml + CreateElement("renderer", std::to_string(renderer));
@@ -153,11 +165,11 @@ int run_rendering_test(ovRenderer renderer, int fullscreen, int width, int heigh
 	{
 		szXml = szXml + CreateElement("test", "3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5");
 	}
-	else if (renderer == RENDERER_VK1)
+	else if (renderer >= RENDERER_VK1_0)
 	{
-		szXml = szXml + CreateElement("test", "1.1");
+		szXml = szXml + CreateElement("test", "1.0");
 	}
-	szXml = szXml + CreateElement("fbenable", "Simple");
+	szXml = szXml + CreateElement("fbenable", "Default");
 	szXml = szXml + CreateElement("fbformat", fbformat);
 	szXml = szXml + CreateElement("scene", scene);
 	szXml = szXml + CreateElement("width", std::to_string(width));
@@ -171,24 +183,13 @@ int run_rendering_test(ovRenderer renderer, int fullscreen, int width, int heigh
 		SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
 	}
 	szXml = CreateElement("root", szXml);
-	/*
-	szXml = "<root><option>1061920</option><duration>20</duration><multisample>8</multisample><anisotropy>0</anisotropy><texturelod>0</texturelod><displaymode>48</displaymode><renderer>12</renderer><pixelformat>1</pixelformat><test>4.2;4.3;4.4;4.5</tet><fbenable>Simple</fbenable><fbformat>RGB</fbformat><scene>0</scene><width>1920</width><height>1080</height></root>";
-	szXml = "<root><test>3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5</test><option>0</option><anisotropy>0</anisotropy><multisample>0</multisample><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode></root>";
-	szXml = "<root><test>4.2;4.3;4.4;4.5</test><option>0</option><anisotropy>0</anisotropy><multisample>8</multisample><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode><width>1920</width><height>1080</height></root>";
-	*/
-	// BUG
-	szXml = "<root><test>3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5</test><option>1048576</option><anisotropy>0</anisotropy><multisample>0</multisample><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode><width>1920</width><height>1080</height></root>";
-
-	// BUG
-//	szXml = "<root><test>3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5</test><option>1061920</option><anisotropy>0</anisotropy><multisample>0</multisample><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode><width>1920</width><height>1080</height></root>";
-
-	// GOOD
-	szXml = "<root><option>1061920</option><duration>20</duration><multisample>8</multisample><anisotropy>0</anisotropy><texturelod>0</texturelod><displaymode>48</displaymode><renderer>12</renderer><pixelformat>1</pixelformat><test>3.0;3.1;3.2;3.3;4.04.1;4.2;4.3;4.4;4.5</test><fbenable>Simple</fbenable><fbformat>RGB</fbformat><scene>0</scene><width>1920</width><height>1080</height></root>";
-
-	// TEST
-	szXml = "<root><test>3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5</test><option>1061920</option><anisotropy>0</anisotropy><multisample>0</multisample><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode><width>1920</width><height>1080</height></root>";
-
-
+	//	szXml = "<root><option>1061920</option><duration>20</duration><sampleCount>8</sampleCount><maxAnisotropy>0</maxAnisotropy><texturelod>0</texturelod><displaymode>48</displaymode><renderer>12</renderer><pixelformat>1</pixelformat><test>4.2;4.3;4.4;4.5</tet><fbenable>Simple</fbenable><fbformat>RGB</fbformat><scene>0</scene><width>1920</width><height>1080</height></root>";
+	//	szXml = "<root><test>3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5</test><option>0</option><maxAnisotropy>0</maxAnisotropy><sampleCount>0</sampleCount><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode></root>";
+	//	szXml = "<root><test>4.2;4.3;4.4;4.5</test><option>0</option><maxAnisotropy>0</maxAnisotropy><sampleCount>8</sampleCount><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode><width>1920</width><height>1080</height></root>";
+	//	szXml = "<root><test>3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5</test><option>1048576</option><maxAnisotropy>0</maxAnisotropy><sampleCount>0</sampleCount><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode><width>1920</width><height>1080</height></root>";
+	//	szXml = "<root><test>3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5</test><option>1061920</option><maxAnisotropy>0</maxAnisotropy><sampleCount>0</sampleCount><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode><width>1920</width><height>1080</height></root>";
+	//	szXml = "<root><option>1061920</option><duration>20</duration><sampleCount>8</sampleCount><maxAnisotropy>0</maxAnisotropy><texturelod>0</texturelod><displaymode>48</displaymode><renderer>12</renderer><pixelformat>1</pixelformat><test>3.0;3.1;3.2;3.3;4.04.1;4.2;4.3;4.4;4.5</test><fbenable>Simple</fbenable><fbformat>RGB</fbformat><scene>0</scene><width>1920</width><height>1080</height></root>";
+	//	szXml = "<root><test>3.0;3.1;3.2;3.3;4.0;4.1;4.2;4.3;4.4;4.5</test><option>1061920</option><maxAnisotropy>0</maxAnisotropy><sampleCount>0</sampleCount><texturelod>0</texturelod><renderer>12</renderer><pixelformat>1</pixelformat><fbenable>Simple</fbenable><duration>6</duration><scene>0</scene><fbformat>RGB</fbformat><displaymode>72</displaymode><width>1920</width><height>1080</height></root>";
 	auto library = LoadLibraryA("infogl.dll");
 	if (library == NULL) {
 		Log.e("Missing DLL infogl.dll");
@@ -247,37 +248,30 @@ int PASCAL WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 	PFNOEVINITWAD funcInitWad = (PFNOEVINITWAD)GetProcAddress(library, "oevInitWad");
 	PNFNOEVRUNRENDERINGTESTS funcRunRenderingTests = (PNFNOEVRUNRENDERINGTESTS)GetProcAddress(library, "oevRunRenderingTests");
-
-
-
-
 	if (funcInitWad("GLVIEW.RMX") >= 0)
 	{
-		/*
-			enum ovRenderer
-			{
-				RENDERER_GDI,
-				RENDERER_GL2_0,
-				RENDERER_GL3_0,
-				RENDERER_GL3_1,
-				RENDERER_GL3_2,
-				RENDERER_GL3_3,
-				RENDERER_GL4_0,
-				RENDERER_GL4_1,
-				RENDERER_GL4_2,
-				RENDERER_GL4_3,
-				RENDERER_GL4_4,
-				RENDERER_GL4_5,
-				RENDERER_GL4_6,
-				RENDERER_VK1,
-				MAX_RENDERER
-			};
-		*/
-		// auto renderer = RENDERER_GDI;
-		//auto renderer = RENDERER_GL2_0;		
-		auto renderer = RENDERER_GL4_6;
-		// auto renderer = RENDERER_VK1;
-		return run_rendering_test(renderer, 0, 1920, 1080, 1, 1, 1, 1, "8", "0", "0", "1", "3", "RGB", 20);
+		// @Note: Choose renderer from here
+		//auto renderer = RENDERER_GDI;
+		//auto renderer = RENDERER_GL2_0; 
+		//auto renderer = RENDERER_GL4_6;
+		auto renderer = RENDERER_VK1_2;
+		return run_rendering_test(renderer,
+			FALSE, // Debug
+			FALSE, // fullscreen
+			1920,
+			1080,
+			0, // vsync
+			0, // fog
+			0, // transp
+			0, // user clip plane			
+			"8", // multisampling
+			"16", // maxAnisotropy
+			"0", // texture_LOD
+			"1", // pixel_format
+			"0", // scene
+			"sRGB",  // fbformat : "Linear", "sRGB", "HDR"
+			20 // duration
+		);
 	}
 	else
 	{
